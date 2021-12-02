ef0f0cf4a075ae6a6d0a52c7e407f4aa0f25cb55
Latifa M <94848902+l-atifa@users.noreply.github.com>
2021-12-02 12:39:24 +0000

    changed instance data, updated accessor and mutator methods accordingly. created new methods

--- Bird.java
+++ Bird.java
@@ -1,24 +1,25 @@
-public class Bird{
+public class Bird
+{
 
 //Instance data
 private String species;
-private double wingspan;
-private int lifespan;
-private boolean layEggs;
+private double beakLength;
+private int age;
+private boolean genderF; //female = true
 
 //Constructor
-public Bird (String sp, double w, int l, boolean e)
+public Bird (String sp, double b, int a, boolean g)
 {
   setSpecies(sp);
-  setWingspan(w);
-  setLifespan(l);
-  setLayEggs(e);
+  setBeakLength(b);
+  setAge(a);
+  setGenderF(g);
 }
 
 //Default Constructor
 public Bird()
 {
-  this("Pigeon", 33.42, 2, false);
+  this("Pigeon", 3.42, 2, false);
 }
 
 //accessor methods
@@ -26,17 +27,17 @@ public String getSpecies()
 {
   return species;
 }
-public double getWingspan()
+public double getBeakLength()
 {
-  return wingspan;
+  return beakLength;
 }
-public int getLifeSpan()
+public int getAge()
 {
-  return lifepsan;
+  return age;
 }
-public boolean getLayEggs()
+public boolean getGenderF()
 {
-  return layEggs;
+  return genderF;
 }
 
 //mutator methods
@@ -44,23 +45,37 @@ public void setSpecies(String sp)
 {
   species = sp;
 }
-public void setWingspan(double w)
+public void setBeakLength(double b)
 {
-  wingspan = w;
+  beakLength = b;
 }
-public void setLifespan(int l)
+public void setAge(int a)
 {
-  lifespan = l;
+  age = a;
 }
-public void setLayEggs(boolean e)
+public void setGenderF(boolean g)
 {
-  layEggs = e;
+  genderF = g;
 }
 
 //other methods
+public String migrate()
+{
+  
+}
 //what would i want to do with a bird:
 //migrate
+
+
+
+public int layEggs()//what would it take in
 //lay getEggs
+{
+  if(layEggs)
+  {
+    Random number 1 - 10
+  }
+}
 //if its true
 
 //toString
@@ -76,3 +91,34 @@ public void setLayEggs(boolean e)
 
 
 }
+
+
+
+
+
+
+You will be designing a class that will demonstrate a variety of the topics discussed in this Unit
+
+-At least 4 pieces of instance data using at least three different variable types (private)
+
+-At least 2 constructors one of which must be a Default Constructor that uses the this command to populate the instance data.  
+
+- accessor methods for all of your instance data
+
+- mutator methods for all of your instance data
+
+- 2 additional methods of your choosing (Example: roll from the Die class)
+
+- an equals method that determines what it means for two objects from your class to be equal to each other.  This is not considered to be one of your additional methods
+
+- a toString method that prints all of your instance data like we did in the Book class.  This is not considered to be one of your additional methods
+
+In your class, each item listed above must be labeled with comments.  For example, comments for one of the constructors might look like...
+
+// Constructors
+
+// Instance data
+
+- Include a driver program to demonstrate that your class works as stated.  In other words, you must show evidence that all constructors  and methods work as designed. Also, you must print each object demonstrating proper use of the toString method.
+
+In repl.it, download your project as a zip file. Upload the zip file here.
